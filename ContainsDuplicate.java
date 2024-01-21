7f5967e6f3d904ade7fda0d7dc06d46d636ccf3b
Guru Dev <guru01803@gmail.com>
2024-01-21 13:59:07 +0530

    update

---
+++ ContainsDuplicate.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        Solution s = new Solution();
+        boolean found = s.containsDuplicate(a);
+        System.out.print(found);
+        in.close();
+    }
+}
