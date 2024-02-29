f47ad6647bc15d853582694d558422e62a2155aa
Guru Dev <guru01803@gmail.com>
2024-02-29 20:53:19 +0530

    updated

---
+++ Dynamic Programming/CanSum.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class CanSum {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        in.close();
+    }
+}
---
+++ Dynamic Programming/FibonacciMemozation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class FibonacciMemozation{
+    private static HashMap<Integer, Integer> memo=new HashMap<>();
+    public static int fibonacci(int n){
+        if(n==0||n==1){
+            return n;
+        }
+        if(memo.containsKey(n)){
+            return memo.get(n);
+        }
+        int res=fibonacci(n-1)+fibonacci(n-2);
+        memo.put(n,res);
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        System.out.println(fibonacci(n));
+        in.close();
+    }
+}
