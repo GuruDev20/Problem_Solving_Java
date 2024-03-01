a80f7094cfc443f4d5fe0a61014109bfd8a0df12
Guru Dev <guru01803@gmail.com>
2024-03-01 23:06:33 +0530

    updated

--- Dynamic Programming/CanSum.java
+++ Dynamic Programming/CanSum.java
@@ -1,7 +1,40 @@
 import java.util.*;
 public class CanSum {
+    public static boolean canSum(int amount,List<Integer> numbers){
+        return canSum(amount,numbers,new HashMap<>());
+    }
+    public static boolean canSum(int amount,List<Integer> numbers,HashMap<Integer,Boolean>memo){
+        if(amount==0){
+            return true;
+        }
+        if(amount<0){
+            return false;
+        }
+        if(memo.containsKey(amount)){
+            return memo.get(amount);
+        }
+        for(int num:numbers){
+            int subAmount=amount-num;
+            if(canSum(subAmount,numbers,memo)){
+                memo.put(amount,true);
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        memo.put(amount,false);
+        return false;
+    }
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
+        int amount=in.nextInt();
+        int size=in.nextInt();
+        List<Integer> num=new ArrayList<>();
+        for(int i=0;i<size;i++){
+            num.add(in.nextInt());
+        }
+        System.out.println(canSum(amount,num));
         in.close();
     }
 }
---
+++ Dynamic Programming/MInimumCoinSum.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class MInimumCoinSum {
+    public static int minCoinSum(int amount,List<Integer> coins){
+        return minCoinSum(amount,coins,new HashMap<>());
+    }
+    public static int minCoinSum(int amount,List<Integer> coins,HashMap<Integer,Integer> memo){
+        if(amount==0){
+            return 0;
+        }
+        if(amount<0){
+            return -1;
+        }
+        if(memo.containsKey(amount)){
+            return memo.get(amount);
+        }
+        int minCoins=-1;
+        for(int coin:coins){
+            int subAmount=amount-coin;
+            int subCoins=minCoinSum(subAmount,coins);
+            if(subCoins!=-1){
+                int num=subCoins+1;
+                if(num<minCoins || minCoins==-1){
+                    minCoins=num;
+                }
+            }
+        }
+        memo.put(amount,minCoins);
+        return minCoins;
+    }
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int amount=in.nextInt();
+        int size=in.nextInt();
+        List<Integer>num=new ArrayList<>();
+        for(int i=0;i<size;i++){
+            num.add(in.nextInt());
+        }
+        System.out.println(minCoinSum(amount,num));;
+        in.close();
+    }
+}
